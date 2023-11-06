93e8284f03f452540f24699926d22fdd6f496084
MabelZar <mabel.zar.esp@gmail.com>
2023-11-06 13:38:39 +0100

    second save

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let enviar = document.getElementById('idBtn')
+enviar.addEventListener('click', function() {
+  let area = document.getElementById('area')
+  console.info("hola"+area )
+  area.innerHTML = area;
+})
