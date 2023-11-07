d3f7209ac6a402cb4af1998e0dce63e156673218
MabelZar <mabel.zar.esp@gmail.com>
2023-11-07 19:27:43 +0100

    players done

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-//const section=document.querySelector("section")
+window.onload=function(){    
+}
+
+const nom =[];
+
 function names(){
-    let nom =[];
-    let cont="";
     let ingresoName;
     for (let i=0;i<=1;i++){
         ingresoName =prompt("Ingresa tu nombre:"," ");
         nom.push(ingresoName)
-        //nom[i]=ingresoName;
-       cont+=nom[i]+"  ";
-      
-        //espacio del nombre de jugadores
-        const myArticle = document.createElement("article");
-        const play1=document.createElement('p');
-        const play2=document.createElement('p');
-        const play3=document.createElement('p');
-         //creando lista de jugadores
-       // const myList=document.createElement('ul');
-        //asignando valor a mis espacios creados
-        play1.textContent='Jugador Nº1:'+nom[0];
-        play2.textContent='Jugador Nº2:'+nom[1];
-        play3.textContent='Jugador Nº3:'+nom[i];
-
-        myArticle.appendChild(play1);
-        myArticle.appendChild(play2);
-        myArticle.appendChild(play3);
-        
-        //section.appendChild(myArticle)
-
     }
-    
-   document.getElementById("area").innerHTML=cont;
 }
 names()
+
+  
+function mostrarPersonas(){
+    let resultado=document.getElementById('gamers');
+    resultado.innerHTML='';
+
+     for(let persona of nom){
+        let datoNombre=document.createElement('li');
+        datoNombre.innerHTML=persona
+        resultado.appendChild(datoNombre);
+         }
+
+}
+mostrarPersonas()
